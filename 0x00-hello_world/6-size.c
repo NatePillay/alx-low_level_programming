9fadccc744b0b6ce62c75183c36f9b914a389fe9
NatePillay <nathan94pillay@gmail.com>
2022-09-08 14:12:00 -0700

    new test

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *Description: main is to write out the vrious int types
+ *
+ *Return: 0 and answer for various types if successful
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", 1);
+	printf("Size of an int: %d byte(s)", 4);
+	printf("Size of a long int: %d byte(s)", 4);
+	printf("Size of a long long int: %d byte(s)", 8);
+	printf("Size of a float: %d byte(s)\n", 4);
+	return (0);
+}
