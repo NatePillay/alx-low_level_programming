0ced5488b9d7167c232d6932214190c710b2fc05
NatePillay <nathan94pillay@gmail.com>
2022-09-08 15:38:57 -0700

    nice tricks here

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
  * Description: main - we going to display code without printf or puts
  *
  * Return: 1 and the answer to the question
- * 
+ *
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",57);
-	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
