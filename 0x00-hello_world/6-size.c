41419f78f038a2dc7e72844363c0e46993d1892f
NatePillay <nathan94pillay@gmail.com>
2022-09-08 14:14:58 -0700

    new try

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- *Description: main is to write out the vrious int types
+ * Description: main - is to write out the various data types
  *
- *Return: 0 and answer for various types if successful
+ * Return: 0 and answer for various types if successful on new lines
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)", 1);
-	printf("Size of an int: %d byte(s)", 4);
-	printf("Size of a long int: %d byte(s)", 4);
-	printf("Size of a long long int: %d byte(s)", 8);
+	printf("Size of a char: %d byte(s)\n", 1);
+	printf("Size of an int: %d byte(s)\n", 4);
+	printf("Size of a long int: %d byte(s)\n", 4);
+	printf("Size of a long long int: %d byte(s)\n", 8);
 	printf("Size of a float: %d byte(s)\n", 4);
 	return (0);
 }
