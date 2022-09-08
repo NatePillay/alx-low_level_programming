255df25e8bdad42bfc07f152c1b873cc4dc167dc
NatePillay <nathan94pillay@gmail.com>
2022-09-08 13:45:35 -0700

    new commit one time try

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main  - entry point
+ *
+ * Return: 0 if other condition is also passed
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
