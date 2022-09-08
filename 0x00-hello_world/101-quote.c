9aa78da3e5308cf914fe9016ee9d4c81f3668626
NatePillay <nathan94pillay@gmail.com>
2022-09-08 15:33:32 -0700

    new step

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Description: main - we going to display code without printf or puts
+ *
+ * Return: 1 and the answer to the question
+ * 
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",57);
+	
+	return (1);
+}
