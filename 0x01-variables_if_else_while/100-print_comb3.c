ebb053b112a941e1437a0bc37fda8cda42ee6107
NatePillay <nathan94pillay@gmail.com>
2022-09-11 09:28:36 -0700

    new styles

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * Description: main - we going to attempt to create a combo of digits 0-9
+ *
+ * Return: 0 and combo of numbers 0 to 9
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i != j)
+			{
+			putchar('0' + i);
+			putchar('0' + j);
+		}
+			putchar('\n');
+	}
+}
+	putchar('n');
+	return (0);
+	}
+
+
