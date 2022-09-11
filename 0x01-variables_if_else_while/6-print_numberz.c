fbe5c7fd944fed70a12438c4cb02725fabfbfdf0
NatePillay <nathan94pillay@gmail.com>
2022-09-10 23:02:04 -0700

    new upload

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * Description: main - we want to only use putchar to print 1 to 10
+ *
+ * Return; 0 to 10 and a new line
+ */
+
+int main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
