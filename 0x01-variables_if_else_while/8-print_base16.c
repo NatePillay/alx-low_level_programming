3964d380f35c6dd87c9416f28cd2ef65a67063a5
NatePillay <nathan94pillay@gmail.com>
2022-09-11 00:22:16 -0700

    new commit new challenge

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * Description: main - we creating the whole hexidecimal range
+ *
+ * Return: 0 and the number system and new line
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
