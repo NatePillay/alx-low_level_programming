b52a30e9d72c3201209e54b42369599710b889d9
NatePillay <nathan94pillay@gmail.com>
2022-09-11 11:02:24 -0700

    new try once again

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * Description: main - same method as before but now its 3 number combo
+ *
+ * Return: 0 and 3 number combo
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 46; i <= 55; i++)
+	{
+		for (j = 47; j <= 56; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+			if (i < j < k)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 || j != 56 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
