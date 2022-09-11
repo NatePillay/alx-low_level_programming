f0483b9f720baac9a8b72b33cd1becc14629edb5
NatePillay <nathan94pillay@gmail.com>
2022-09-11 11:28:07 -0700

    new new new try

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 47; i <= 58; i++)
+	for (i = 48; i <= 58; i++)
 	{
-		for (j = 48; j <= 58; j++)
+		for (j = 49; j <= 58; j++)
 		{
-			for (k = 49; k <= 58; k++)
+			for (k = 50; k <= 58; k++)
 			{
 			if (i < j && i < k)
 			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if (i != 56 || j != 57 || k != 58)
+				if (i != 55 || j != 56)
 				{
 					putchar(',');
 					putchar(' ');
