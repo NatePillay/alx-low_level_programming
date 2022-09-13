3d307fb33fba99ade93cb38b761af6112541fb30
NatePillay <nathan94pillay@gmail.com>
2022-09-13 12:31:05 -0700

    new commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <_putchar>
+/**
+ * Description: main - Printing _putchar today
+ *
+ * Return: _putchar and 0 if successful
+ */
+int main(void)
+{
+	char n[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
