ab9b4786bdc6bf8522986fa0c4771983cb504f52
NatePillay <nathan94pillay@gmail.com>
2022-09-13 14:08:04 -0700

    new commit'
    d
    escape

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10x
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	if (i <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	} i++;
+	_putchar('\n');
+}
