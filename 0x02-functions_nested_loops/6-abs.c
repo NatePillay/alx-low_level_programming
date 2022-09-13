ea0f0ac2c3750e9a5086f80b93839c1e10d64f41
NatePillay <nathan94pillay@gmail.com>
2022-09-13 16:22:13 -0700

    new try

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  *_abs - this function will be used to get absolute value
- *@int: this is the data type of the answer we want returns
+ *@n: this is the data type of the answer we want returns
  *Returns: _abs number if success 0 if nothing
  */
-int _abs(int)
+int _abs(int n)
 {
 	int n;
 
