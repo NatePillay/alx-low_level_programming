48ae486af424a180cba4aa20d4fe1a01628ba788
NatePillay <nathan94pillay@gmail.com>
2022-09-13 19:36:09 -0700

    new commits

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,8 +22,8 @@ void times_table(void)
 			}
 			if (j >= 10)
 			{
-				_putchar((j / 10) + '0');
-				_putchar((j / 10) + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 			else if (k < 10 && j != 0)
 			{
