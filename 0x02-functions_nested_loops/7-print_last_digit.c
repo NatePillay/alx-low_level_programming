0ebb69975ae473dd8fda2820c862b3a80222590d
NatePillay <nathan94pillay@gmail.com>
2022-09-13 17:06:08 -0700

    new commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,12 @@
 /**
  * print_last_digit - this function will find last digit
  * @n: paramter we give our functon
- * @i: used for getting the modular
  * Return: to get the result of print_last_digit
  */
-int print_last_digit(int n, int i)
+int print_last_digit(int n)
 {
+	i = 0;
+
 	i = n % 10;
 	return (i);
 }
