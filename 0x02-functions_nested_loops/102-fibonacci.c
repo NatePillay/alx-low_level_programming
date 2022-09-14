b65ef5eaec65d47e1685255fa63b6ddfed59d76c
NatePillay <nathan94pillay@gmail.com>
2022-09-14 15:04:42 -0700

    new try

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,18 @@
 int main(void)
 {
 	int i, n;
-	int t1 = 0, t2 = 1;
+	int t1 = 1, t2 = 2;
 	int nextTerm = t1 + t2;
 
 	n = 50;
 
 
-	for (i = 3; i <= n; i++)
+	for (i = 1; i <= n; i++)
 	{
 		printf("%d, ", nextTerm);
+		nextTerm = t1 + t2;
 		t1 = t2;
 		t2 = nextTerm;
-		nextTerm = t1 + t2;
 	}
 	return (0);
 }
