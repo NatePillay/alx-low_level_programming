420869dbeba7415b992ba191b1a9ec828b37070f
NatePillay <nathan94pillay@gmail.com>
2022-09-13 17:02:59 -0700

    new attempt

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - this function will find last digit
+ * @n: paramter we give our functon
+ * @i: used for getting the modular
+ * Return: to get the result of print_last_digit
+ */
+int print_last_digit(int n, int i)
+{
+	i = n % 10;
+	return (i);
+}
