60c37fb94365fa55c408cef1a36c26af62c488f5
NatePillay <nathan94pillay@gmail.com>
2022-09-13 17:27:50 -0700

    new commit session

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,19 @@
 /**
  * print_last_digit - this function will find last digit
  * @n: paramter we give our functon
- * @i: this parameter needed for the code
  * Return: to get the result of print_last_digit
  */
-int print_last_digit(int n, int i)
+int print_last_digit(int n)
 {
-	i = n % 10;
-	return (i);
+	if (n > 0 || n == 0)
+	{
+		_putchar(n % 10 + '0');
+		return (n % 10);
+	}
+	else
+	{
+		n = (n * (-1));
+		_putchar(n % 10 + '0');
+		return (n % 10);
+	}
 }
