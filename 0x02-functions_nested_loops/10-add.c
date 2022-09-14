32f0cd568ae66a67e7df1c799d0d28391746196d
NatePillay <nathan94pillay@gmail.com>
2022-09-13 19:24:01 -0700

    new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds the two together in function
+ * @x: first number to be added
+ * @i: second number to be added
+ * Return: result of two integers adds.
+ */
+int add(int x, int i)
+{
+	int x;
+	int i;
+
+	return (x + i);
+}
