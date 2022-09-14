757b0a816c64f923404ec47a9873a4a1f923cbe0
NatePillay <nathan94pillay@gmail.com>
2022-09-13 17:11:00 -0700

    new commi

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,11 @@
 /**
  * print_last_digit - this function will find last digit
  * @n: paramter we give our functon
+ * @i: this parameter needed for the code
  * Return: to get the result of print_last_digit
  */
-int print_last_digit(int n)
+int print_last_digit(int n, int i)
 {
-	i = 0;
-
 	i = n % 10;
 	return (i);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,4 +7,4 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-void print_to_98(int n);
+void print_to_98(int n, int i);
