f74b3633356fd09d6516abc76ab4814056cd5210
NatePillay <nathan94pillay@gmail.com>
2022-09-14 15:50:06 -0700

    new new ne commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * Description: main - we going to sum up all even numbers
+ *
+ * Return: we only want even numbers
+ */
+int main(void)
+{
+	long int i, n, sum;
+	long int t1 = 1, t2 = 2;
+	long int term = t1 + t2;
+
+	n = 33;
+
+	for (i = 1;  i <= 33; ++i)
+	{
+		if (i < 40000000 && (i % 2) == o)
+		{
+			sum = i + sum;
+		}
+		term = t1 + t2;
+		t2 = t1;
+		t2 = term;
+	}
+	return (0);
+}
