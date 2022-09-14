3770fae70431c165d2ed5ef841d0e5c3f6bf6d96
NatePillay <nathan94pillay@gmail.com>
2022-09-14 14:06:18 -0700

    new commit tonight

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,16 +9,18 @@ void print_times_table(int n)
 	int j;
 	int i;
 
-	if (n > 0 || n < 15)
+	if (!(n < 0 || n > 15))
 	{
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
-				i = i * n;
-				j = j * n;
-				putchar(i * j + ' ');
-				putchar(';');
+				k = i * j;
+				if (j != 0)
+				{
+					putchar(' ');
+					putchar(';');
+				}
 			}
 			putchar('\n');
 		}
