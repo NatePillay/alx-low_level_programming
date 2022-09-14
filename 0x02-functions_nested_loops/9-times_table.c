f1876df6d76fd2df11836cb691a21858a9c3ffdf
NatePillay <nathan94pillay@gmail.com>
2022-09-13 19:05:23 -0700

    new new commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - function to add up time tables
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			_putchar(k);
+		}
+		printf('\n');
+	}
+}
