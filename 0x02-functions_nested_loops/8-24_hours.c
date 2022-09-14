99462e7f4f7a307fe304d3606b9aeeb53b34c945
NatePillay <nathan94pillay@gmail.com>
2022-09-13 18:22:47 -0700

    new commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - this function will display all time
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 59; j++)
+	{
+		for (i = 0; i <= 23; i++)
+		{
+			_putchar(i + '0');
+			_putchar(j + '0');
+			_putchar('\n');
+		}
+	}
+}
