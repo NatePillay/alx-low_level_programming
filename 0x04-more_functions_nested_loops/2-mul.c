adae3997c67d9c99a0b890ca4707169a7cb2b51d
NatePillay <nathan94pillay@gmail.com>
2022-09-15 13:37:58 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function to multiple
+ * @a: first variable
+ * @b: second variable
+ * Return: sum of the two
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
