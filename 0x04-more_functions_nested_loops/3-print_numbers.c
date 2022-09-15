e98f4603e38fa2f7feb4fa6b3bafd4c9c2712b54
NatePillay <nathan94pillay@gmail.com>
2022-09-15 13:45:41 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - function to return numberz
+ *
+ * Return: 0 if success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
