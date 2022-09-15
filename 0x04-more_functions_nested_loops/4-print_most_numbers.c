b2adc40e590eed8cfccadf9a512251724003ab92
NatePillay <nathan94pillay@gmail.com>
2022-09-15 15:00:23 -0700

    new commit, slowly draining

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,18 @@
  */
 void print_most_numbers(void);
 {
-	char c;
+	char c = '0';
 
-	for (c = '0'; c <= '9'; c++)
+	while (c <= '9')
 	{
-		if (!(c == '2' || c == '4'))
+		if (c != '2' && c != '4')
 		{
 			_putchar(c);
 		}
+	c++;
+
 	}
+
 	_putchar('\n');
 
 }
