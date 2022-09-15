379d2a8381805e07373ca4f1594edcfc0be2123e
NatePillay <nathan94pillay@gmail.com>
2022-09-15 12:09:06 -0700

    new commit today

---
+++ 0x03-debugging/o-main.C
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test the function using a test case
+ * Return: 0
+ */
+
+int main()
+{
+	int i = 0;
+	int checker;
+
+	checker = positive_or_negative(i)
+		return (0);
+}
