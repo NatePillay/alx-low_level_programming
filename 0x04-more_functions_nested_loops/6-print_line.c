4f67cc4da114609f207b661eb237c90c29aa3e5e
NatePillay <nathan94pillay@gmail.com>
2022-09-15 15:54:19 -0700

    touch up

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - function to print a line of _
+ * @n: n will act as the number of integers
+ * Return: 0 and a line
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar("_");
+		}
+		_putchar('\n');
+
+	}
+	return (0);
+}
+
