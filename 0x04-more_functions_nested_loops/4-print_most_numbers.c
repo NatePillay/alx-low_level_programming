ddfbd854659d171b0fea2c092239052cc1bb17a8
NatePillay <nathan94pillay@gmail.com>
2022-09-15 14:55:36 -0700

    new trades

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,16 @@
  *
  * Return: 0
  */
-void print_most_numbers(void)
+void print_most_numbers(void);
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c)
+		if (!(c == '2' || c == '4'))
+		{
+			_putchar(c);
+		}
 	}
 	_putchar('\n');
 
