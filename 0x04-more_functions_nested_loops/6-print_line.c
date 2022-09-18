175a3f2f13c1d71875c218310c4eb8782a998f0e
NatePillay <nathan94pillay@gmail.com>
2022-09-18 00:17:26 -0700

    new commit, keepi it simple

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,18 +8,14 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (n  > 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			_putchar(95);
 		}
-		_putchar('\n');
-
 	}
+	putchar('\n');
 }
+
 
