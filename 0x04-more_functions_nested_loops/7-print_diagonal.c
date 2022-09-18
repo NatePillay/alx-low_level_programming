22d62f238da380d264d2c1d7891c6636508f4928
NatePillay <nathan94pillay@gmail.com>
2022-09-18 00:56:33 -0700

    new commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,27 +9,26 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				if (i == j)
 				{
-					_putchar('\\');
+					_putchar(92);
 				}
-				else if (j < i)
+				else
 				{
 					_putchar(' ');
 				}
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
+	_putchar('\n');
+	return (0);
 }
+
 
