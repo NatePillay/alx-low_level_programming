752027990ef84ea36c1ce28ab9fb6f715f3a6807
NatePillay <nathan94pillay@gmail.com>
2022-09-18 03:24:29 -0700

    new commit line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,11 +9,7 @@ void print_triangle(int size)
 {
 	int i, j, s;
 
-	if (size <= 0)
-	{
-		_putchar('\n')
-	}
-	else
+	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
 		{
@@ -21,11 +17,15 @@ void print_triangle(int size)
 			{
 				_putchar(' ');
 			}
-			for (j = 1; j <= size; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		putchar('\n');
 	}
 }
