f47efeea5e38f42874ac1a12c2eda8ef1e3f1eba
NatePillay <nathan94pillay@gmail.com>
2022-09-19 09:36:51 -0700

    new commit my type

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to return a value to a pointer
+ * @n: variable 1
+ * Return: 0 if success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,7 @@
 #include <stdio.h>
 
 int _putchar(int c);
+void reset_to_98(int *n);
+
 
 #endif
