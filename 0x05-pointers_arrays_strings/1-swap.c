c971fc60d2ad198deb46c8a1af1d48f28b89eb28
NatePillay <nathan94pillay@gmail.com>
2022-09-19 14:26:34 -0700

    new commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,17 +4,14 @@
  * swap_int - this function will swap them around
  * @a: varaible 1
  * @b: variable 2
- * @c: variable additional
  * Return: 0 if success
  */
 
 void swap_int(int *a, int *b)
 {
-	int c; 
-		
+	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
-
-	return;
 }
