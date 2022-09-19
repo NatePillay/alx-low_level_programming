01ab776b0d87f8ade6e476efa331f9b746e111bf
NatePillay <nathan94pillay@gmail.com>
2022-09-19 15:02:51 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - function print out some words
+ * @str: 1 variable
+ * Retur: should return a list of characters
+ */
+
+void puts2(char *str)
+{
+	while (str != '\0')
+	{
+		_putchar(*str);
+		str = str + 2;
+	}
+	_putchar('\n');
+}
