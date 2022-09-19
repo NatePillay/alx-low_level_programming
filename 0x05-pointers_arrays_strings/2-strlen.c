184235529a94cec5cf154f9aba38f2ed0df441fd
NatePillay <nathan94pillay@gmail.com>
2022-09-19 11:19:36 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function to find string length
+ * @s: 1st variable
+ * Return: 0 if success
+ */
+int _strlen(char *s)
+{
+
+	unsigned int count = 0;
+
+	while (*s != 0)
+	{
+		count++;
+		*s++;
+	}
+}
