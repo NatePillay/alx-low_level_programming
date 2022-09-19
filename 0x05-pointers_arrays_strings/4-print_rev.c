32a7ab45802ad6a1e1059ee2e57600374e5fe489
NatePillay <nathan94pillay@gmail.com>
2022-09-19 12:21:30 -0700

    new line commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - function to reverse string
+ * @s: first variable
+ * Return: 0 if success
+ */
+void print_rev(char *s);
+{
+	int i, str, count;
+
+	count  = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+	}
+
+	for (i = 0; i < count / 2; i++)
+	{
+		str = *s[i];
+		*s[i] = *s[count - i - 1];
+		*s[count - i - 1] = str;
+	}
+
+}
