dd688df97c5778b95d080ade96989cd209d2c793
NatePillay <nathan94pillay@gmail.com>
2022-09-19 09:42:00 -0700

    new commit line

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - this function will swap them around
+ * @a: varaible 1
+ * @b: variable 2
+ * Return: 0 if success
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
