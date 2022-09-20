be59e4b5e8fd2ce0b37155cfd531ba1067d5db5c
NatePillay <nathan94pillay@gmail.com>
2022-09-19 17:26:17 -0700

    new commit line

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _atoi - function to convert string to int
+ * @s: 1 var
+ * Return: 0
+ */
+
+int _atoi(char *s)
+{
+	int res = 0;
+
+	for (int i = 0; s[i] != '\0'; ++i)
+		res = res * 10 + s[i] - '0';
+
+	return (res);
+}
