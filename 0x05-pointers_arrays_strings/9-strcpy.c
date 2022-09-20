3e8c743f40188e710158843db7f63fff0d101654
NatePillay <nathan94pillay@gmail.com>
2022-09-19 17:12:03 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function to copy
+ * @dest: var 1
+ * @src: var 2
+ * Return: copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0'; ++i)
+	{
+		src[i] = dest[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
