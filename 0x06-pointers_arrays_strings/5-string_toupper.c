6470793dc10ccde8490e0107b25cc1ad9ec21148
NatePillay <nathan94pillay@gmail.com>
2022-09-22 10:55:48 -0700

    new commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - convert string to upper
+ * @s1: variable to represent the string
+ * Return: upper case letters
+ */
+
+char *string_toupper(char *s1)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] >= 97 && s1[i] <= 122)
+		{
+			s1[i] = s1[i] - 32;
+		}
+	}
+	return (s1);
+}
