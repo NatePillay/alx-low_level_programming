9c454e1f0528f969292fa2a8973f2240e5c40a76
NatePillay <nathan94pillay@gmail.com>
2022-09-22 13:55:18 -0700

    new commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,31 @@
 #include "main.h"
 
 /**
- * _strncpy - function to copy contents
- * @dest: destination variable
- * @src: source variable
- * @n: counter
- * Return: merged string if success
+ *_strncpy - this is a copy function we create
+ * @dest: destination var
+ * @src: source var
+ * @n: counting var
+ * Return: copy of string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	int i = 0;
+	char *return_ptr = dest;
+
+	while (*src != '\0' &&  i < n)
 	{
-		dest[i] = src[i];
+		*dest = *src;
+		*dest++;
+		*src++;
+		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	*dest = '\0';
+
+	return (return_ptr);
 }
+
+
+
+
