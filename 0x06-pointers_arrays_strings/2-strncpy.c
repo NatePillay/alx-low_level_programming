ae02babab2d6f6462f95ff170e6d3070a986030e
NatePillay <nathan94pillay@gmail.com>
2022-09-22 14:04:52 -0700

    new commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,10 +16,9 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		dest[i] = '\0'';
-		i++;
+		dest[i] = '\0';
 	}
 
 	return (dest);
