4d98e1b4d49afcc8ddc5b1e4a9af88fae62e4744
NatePillay <nathan94pillay@gmail.com>
2022-09-22 11:06:21 -0700

    new commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,12 +11,14 @@ char *string_toupper(char *s1)
 	int i;
 	int len = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	while (s[i++] != '\0')
 	{
 		if (s1[i] >= 97 && s1[i] <= 122)
 		{
 			s1[i] = s1[i] - 32;
 		}
+		len++;
+
 	}
 	return (s1);
 }
