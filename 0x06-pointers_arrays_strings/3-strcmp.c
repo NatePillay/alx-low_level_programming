8b4a595e7de7aea09b80e817a67e3c07214edbb5
NatePillay <nathan94pillay@gmail.com>
2022-09-22 14:11:26 -0700

    new commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int flag = 0;
+	int flag;
 	int i;
+
+	flag  = 0;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			flag = 1;
+			flag = *(s1 + i);
 			break;
 		}
 	}
-	if (flag == 0)
-	{
-		return (0);
-	}
+	return (flag);
 }
