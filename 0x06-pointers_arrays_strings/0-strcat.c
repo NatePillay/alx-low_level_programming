c35c90e82cf385f67bfd0bca0e89a71d9897fdaf
NatePillay <nathan94pillay@gmail.com>
2022-09-22 09:57:24 -0700

    New commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - function to concatnate two strings
+ * @dest: variable dest
+ * @src: variable src
+ * Return: return 1 string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int count = 0;
+	int i;
+
+	while (*dest != '\0')
+	{
+		count++;
+		dest++;
+	}
+
+	for (i = 0; i <= count; i++)
+	{
+		dest[count + i] + src[i];
+	}
+	dest = '\0';
+	return(dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+
+char *_strcat(char *dest, char *src);
+
+#endif
