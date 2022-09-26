324c548de070a9d84403dde536f9153d71a02404
NatePillay <nathan94pillay@gmail.com>
2022-09-26 20:42:49 +0200

    new commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - to copy a data from source to dest for number of characters
+ * @dest: destination var
+ * @src: source var
+ * @n: number of charcteras
+ * Return: the selected source data copied to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+    char *destin = (char *)dest;
+    char *source = (char *)src;
+
+    for (int i = 0; i < n; i++)
+    {
+        destin[i] = source[i];
+    }
+}
