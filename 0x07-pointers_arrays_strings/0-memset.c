5a7dc69af9cd020e39ceed866cbb4998dfa49df5
NatePillay <nathan94pillay@gmail.com>
2022-09-26 19:09:57 +0200

    new commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * _memset - function to copy another
+ * _memset - function to fill n bytes of memory area pointed to
+ * by @s with constant byte @b
  * @s: character string
  * @b: what we want returned
  * @n: counting variable
- * Return: string s
+ * Return: a pointer to the memory area @s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    int i;
+    unsigned int i;
 
     for (i = 0; i < n; i++)
     {
