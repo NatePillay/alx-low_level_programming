d191b9aed1ea44ecb9032d484ddc814da66a0748
NatePillay <nathan94pillay@gmail.com>
2022-09-26 14:11:24 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function to return the string and first char
+ * @s: string variable
+ * @accept: string variable to accept
+ * Return: a pointer to bytes in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	do {
+		if (*s == *accept)
+		{
+			return (s);
+		}
+	} while (*s++);
+	return (s);
+}
