90d99204143c8229e23152b708224162ba1f6aef
NatePillay <nathan94pillay@gmail.com>
2022-09-26 15:13:23 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function will print all values of chess board
+ * @a: character variable
+ * Return: the print of chess board
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (int i = 0; i <= 7; i++)
+	{
+		for (int j = 0; j <= 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
+
