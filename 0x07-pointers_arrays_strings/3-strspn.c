0efaf4e05bc67a31757951bd36a66064e1799de5
NatePillay <nathan94pillay@gmail.com>
2022-09-26 22:28:41 +0200

    new commit wave

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - this will return the number of bytes
+ * @s: string array
+ * @accept: characters matched
+ * Return: the count of this
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int len1 = 0;
+    unsigned int len2 = 0;
+    unsigned int count = 0;
+    unsigned int i ,j;
+    
+
+    while (*s != '\0')
+    {
+        len1++
+    }
+
+    while (*accept != '\0')
+    {
+        len2++
+    }
+
+    for (i = 0; i <= len1; i++)
+    {
+        for (j = 0l j <= len2; j++)
+        {
+            *s[i] = *accept[i]
+            count++;
+
+        }
+    }
+    return (count);
+}
