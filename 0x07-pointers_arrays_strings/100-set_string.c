ffc24a8e8689203750fc3d1c5ab8eaa3f0962f40
NatePillay <nathan94pillay@gmail.com>
2022-09-26 16:03:09 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_string - function that returns a pointer
+ * @s: pointer to a pointer
+ * @to: pointer to a string
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+
+	char *to = &s0;
+	char **p = *to;
+
+}
