9c3487a31c041f7981fcfcf6ec507cff27ab8a44
NatePillay <nathan94pillay@gmail.com>
2022-09-28 14:34:48 -0700

    new commit please

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function to be used for square root
+ * @n: variable to root
+ * @x: count variable
+ * Return: integer value
+ */
+
+int _sqrt_recursion(int n, int x)
+{
+
+	if (n / x != n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0 + _sqrt_recursion(n, x + 1));
+	}
+}
