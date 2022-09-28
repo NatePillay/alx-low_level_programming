aa329c2865d31f80593274b89054ca2089bd4e72
NatePillay <nathan94pillay@gmail.com>
2022-09-28 12:54:02 -0700

    new commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,13 +9,12 @@
 int _strlen_recursion(char *s)
 {
 
-	if (*s == '\0')
+	if (*s == 0)
 	{
 		return (0);
 	}
 	else
 	{
-		return (*s + _strlen_recursion(s + 1));
+		return _strlen_recursion(*s++);
 	}
-		
 }
