3898c44b079fba262e59f2a61d5a52882223cc0e
NatePillay <nathan94pillay@gmail.com>
2022-09-28 12:29:07 -0700

    new next commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - we are going to reverse the string
+ * @s: the character variable we shall use
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
