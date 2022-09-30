a90b83a5329d4ba409cc297e486ae251b1ea9735
NatePillay <nathan94pillay@gmail.com>
2022-09-30 05:47:01 -0700

    new commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -function to convert cents to rands
+ * @argc: number of arguments
+ * @argv: number of pointers to char
+ * Return: the converted value
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
+
