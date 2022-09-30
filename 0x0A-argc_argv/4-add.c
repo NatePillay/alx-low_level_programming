0e326d684153e1d2d67fc4ecb83723222c69c2e3
NatePillay <nathan94pillay@gmail.com>
2022-09-30 03:31:08 -0700

    new commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,5 +5,19 @@
  * main - function to sum and print nums
  * @argc: count of arguments
  * @argv: array of pointers to char
+ * Return: sum of ints
  */
 
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argv > 0)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			sum = sum + atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+}
