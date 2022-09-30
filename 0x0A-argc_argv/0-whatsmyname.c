7715332d7c2500d9ba750a4d7ff44adf35611d86
NatePillay <nathan94pillay@gmail.com>
2022-09-30 02:23:52 -0700

    new commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function to return name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: name of the program
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argv[0] = %s\n", *argv);
+	return (0);
+}
+
+
+
