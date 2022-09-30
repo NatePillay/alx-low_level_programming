aa8ec8ad5130976ff84489accac11c7c855cf789
NatePillay <nathan94pillay@gmail.com>
2022-09-30 02:30:59 -0700

    new line commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function to print out number of args
+ * @argc: argument count
+ * @argv: vector of arguments
+ * Return: number of arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
