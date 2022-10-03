da33dd3af133720a88b75e7dbed58e62e4e3f17b
NatePillay <nathan94pillay@gmail.com>
2022-10-03 15:11:18 -0700

    new commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,6 +9,7 @@
 
 char *_strdup(char *str)
 {
+	int i;
 
 	char *c = (char *)malloc(sizeof(char));
 
@@ -16,7 +17,7 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	int i = 0;
+	i = 0;
 
 	while (i != '\0')
 	{
