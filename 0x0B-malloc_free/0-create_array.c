b767a2826606a52dff5a0f18d2ffed0eca9e9fab
NatePillay <nathan94pillay@gmail.com>
2022-10-03 14:25:59 -0700

    new commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - function to save mem for the char
+ * @size: the int var
+ * @c: the char
+ * Return: pointer of array or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+
+	s = malloc((size + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
