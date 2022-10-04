5a25322744b642399a5edeee5d435f9533ec7e03
NatePillay <nathan94pillay@gmail.com>
2022-10-04 16:33:04 -0700

    new commit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * argstostr - function to concatenate the string of vals
+ * @ac: one var
+ * @av: vector array of pointers to string
+ * Return: successful retur the words with space
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int len = 1;
+	char *str;
+
+	if (!ac || av[i] == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 1; i < ac; i++)
+	{
+		len += strlen(av[i]);
+	}
+	str = '\0';
+	str = malloc(sizeof(char) * len);
+
+	for (i = 1; i < ac; i++)
+	{
+		strcat(str, av[i]);
+		_putchar('\n');
+	}
+
+	free(str);
+}
+
