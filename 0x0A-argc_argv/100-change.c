8b35723b8164e287f2c56c5a45998f639e22b502
NatePillay <nathan94pillay@gmail.com>
2022-10-05 15:09:50 -0700

    new commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,6 +10,28 @@
 
 int main(int argc, char *argv[])
 {
+	int cng, i, amount;
+
+	int coins = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+	}
+
+	amount = atoi(argv[1]);
+
+	if (amount < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+
+
+
+
+
 
 }
 
