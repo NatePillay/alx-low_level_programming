699f9e261071ea2b098daec9c815d33a51e7ed9f
NatePillay <nathan94pillay@gmail.com>
2022-10-06 13:05:02 -0700

    new commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function to allocate memory in C
+ * @b: integer type
+ * Return: nothing
+ */
+
+void *malloc_checked(unsigned int b);
+{
+	void *ptr = b;
+
+	if (*ptr == NULL)
+	{
+		return (98);
+	}
+}
