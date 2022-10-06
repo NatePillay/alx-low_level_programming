324881aa7c94bc3984782b1a16e7782f6a0c2185
NatePillay <nathan94pillay@gmail.com>
2022-10-06 14:46:17 -0700

    new connect

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,7 +44,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 
-	for (i = 0; s1[i] != '\0', i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		s[i] = s1[i];
 	}
@@ -52,7 +52,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s[i + j] = s2[j];
 	}
-	s(i + j) = '\0';
+	s[i + j] = '\0';
 
 }
 
