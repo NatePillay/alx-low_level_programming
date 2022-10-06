66d7980696680e222b6a2ef1621adda32de43369
NatePillay <nathan94pillay@gmail.com>
2022-10-06 15:09:12 -0700

    new commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,40 +11,37 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j, len, size;
+	unsigned int len, i;
 	char *s;
 
-	n = size;
+	len = n;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (size < 0)
-		return (NULL);
-	if (size >= strlen(s2))
+	for (i = 0; s1[i]; i++)
 	{
-		size = strlen(s2);
+		len++;
 	}
 
-	len = strlen(s1) + size + 1;
-
-	s = malloc(sizeof(*s) * len);
+	s = malloc(sizeof(char) * (len + 1));
 
 	if (s == NULL)
 	{
 		return (NULL);
 	}
+	len = 0;
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
-		s[i] = s1[i];
+		s[i++] = s1[i];
 	}
-	for (j = 0; j < size ; j++)
+	for (i = 0; s2[i] && i < n; i++)
 	{
-		s[i + j] = s2[j];
+		s[i++] = s2[i];
 	}
-	s[i + j] = '\0';
+	s[len] = '\0';
 
 }
 
