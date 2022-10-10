f55a8f1cf423b17b0427ec224cf08e2b2197e87c
NatePillay <nathan94pillay@gmail.com>
2022-10-10 12:33:04 -0700

    new commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,17 +1,7 @@
 #define _3_FUNCTION_LIKE_MACRO_H_
 #ifndef _3_FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) x * -1
+#define ABS(x) ((x) < (0) ?  (-(x)) (x)) 
 
-int main()
-{
-	int i;
 
-	if (i < 0)
-	{
-		ABS(i)
-	}
-
-	return (i);
-}
 #endif
