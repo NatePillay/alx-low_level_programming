d087a0176803c8c359230b859697f9006e79f722
NatePillay <90217237+NatePillay@users.noreply.github.com>
2022-10-11 01:28:43 +0200

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,48 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 
-#include "main.h"
-
-/**
- * _strlen - function to find string length
- * @s: 1st variable
- * Return: 0 if success
- */
-int _strlen(char *s)
-{
-
-	unsigned int count = 0;
-
-	while (*s != 0)
-	{
-		count++;
-		s++;
-	}
-	return (count);
-}
-
-
-/**
- * *_strcpy - function to copy
- * @dest: var 1
- * @src: var 2
- * Return: copied
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-
-
 /**
  * free_dog - free up memory
  * @d: typedef func
