d62df4c7ea5a00b40c2fc268fbf53169b1b8d2ca
NatePillay <nathan94pillay@gmail.com>
2022-10-10 13:48:53 -0700

    new commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - for dogs
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: struct to store and use info regarding dog
+ */
+
 struct dog
 {
 	char *name;
