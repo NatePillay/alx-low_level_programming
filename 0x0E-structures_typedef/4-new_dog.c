95fd7dd8e1f3cdc44f2b5d4e1fca44b71e12c467
NatePillay <90217237+NatePillay@users.noreply.github.com>
2022-10-11 01:29:16 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,46 @@
 #include "dog.h"
 #include <stdlib.h>
 
+
+/**
+ * _strlen - function to find string length
+ * @s: 1st variable
+ * Return: 0 if success
+ */
+int _strlen(char *s)
+{
+
+	unsigned int count = 0;
+
+	while (*s != 0)
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
+
+/**
+ * *_strcpy - function to copy
+ * @dest: var 1
+ * @src: var 2
+ * Return: copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+
 /**
  * new_dog - type def function to store new data
  * @name: var 1
