6118a81e9cbc45d9ec79c40c90675f13ea33d037
NatePillay <nathan94pillay@gmail.com>
2022-10-13 13:44:52 -0700

    new commit new commt

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - function to print string
+ * @separator: var 1
+ * @n: var 2
+ * Return: nothing void func
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	char *s;
+	unsigned int i;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(list, char *);
+
+		if (s == NULL)
+		{
+			printf("NIL");
+		}
+		else
+		{
+			printf("%s", s);
+		}
+
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(list);
+	printf("\n");
+}
