bda5b00e81dc37d8f7680beaea8805624c86f7a3
NatePillay <nathan94pillay@gmail.com>
2022-10-12 20:37:27 -0700

    new commit commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function to return occurences
+ * @array: the array
+ * @size: size of grid
+ * @cmp:pointer to function
+ * Return: returns integer value
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+}
