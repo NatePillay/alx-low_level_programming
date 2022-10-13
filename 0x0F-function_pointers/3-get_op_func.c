c3e4c60dc45def5820c94ad95e7b9e8ffc23bae8
NatePillay <nathan94pillay@gmail.com>
2022-10-13 06:44:29 +0200

    new commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,6 +9,13 @@ int (*get_op_func(char *s))(int, int)
         {NULL, NULL}
     };
     int i;
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
 
-    
+		i++;
+	}
+
+	return (0);
 }
