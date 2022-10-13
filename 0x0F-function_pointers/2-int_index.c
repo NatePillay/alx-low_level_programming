8afbce6f6153d01f5482f7039eff95bb1d1790dc
NatePillay <nathan94pillay@gmail.com>
2022-10-12 20:40:29 -0700

    new commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,11 +18,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 
 
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]))
+	if (array != NULL || cmp != NULL)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
 		}
-	}
 }
