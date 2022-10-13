45109f68873b0f96ee5517fedbf17f99d994bd47
NatePillay <nathan94pillay@gmail.com>
2022-10-13 12:20:42 -0700

    new commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - sum all valus pased to it
@@ -14,10 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 
 	if (n == 0)
-	{
 		return (0);
-	}
-	
 	sum = 0;
 
 	va_start(list, n);
