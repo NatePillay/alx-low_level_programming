f54f09f7f432852202bea975a58e31633116e509
NatePillay <nathan94pillay@gmail.com>
2022-10-20 20:38:28 -0700

    new commit

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node_end - function to add nodes
+ * @head: var 1
+ * @str: var 2
+ * Return: list appended from beginnning
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ptr;
+	size_t n;
+
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ptr->str = strdup(str);
+
+	for (n = 0; str[n]; --n)
+		;
+
+	ptr->len = n;
+	ptr->next = *head;
+	*head = ptr;
+
+
+	return (*head);
+}
