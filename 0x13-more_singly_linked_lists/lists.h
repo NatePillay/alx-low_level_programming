2d24206658605b3b578d135ba1ec13a3cda38751
NatePillay <90217237+NatePillay@users.noreply.github.com>
2022-10-24 11:51:37 +0200

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,11 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t print_listint(const listint_t *h);
-
-
-
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -23,6 +18,10 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+
+
+
+size_t print_listint(const listint_t *h);
 
 
 #endif
