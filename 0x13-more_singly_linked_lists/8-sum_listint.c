1923d7b28064b169ef810452819b9b64acf1fe91
NatePillay <nathan94pillay@gmail.com>
2022-10-25 14:30:18 -0700

    new commit

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - a function to sum all members of list
+ * @head: pointer to head
+ * Return: integer value from sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum;
+	listint_t *tmp;
+
+	tmp = head;
+	sum = 0;
+
+	while (tmp != NULL)
+	{
+		sum += head->n;
+		tmp = tmp->next;
+	}
+
+	return (sum);
+}
