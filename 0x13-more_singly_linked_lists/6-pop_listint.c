68dd668337320f969885c152fe488b2456affcfb
NatePillay <nathan94pillay@gmail.com>
2022-10-25 12:52:35 -0700

    new commit challenge

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,12 +3,15 @@
 /**
  * pop_listint - function to pop the end off
  * @head: point to pointer
- * Return: removes the int
+ * Return: head node's data or 0 if ll empty
  */
 
 int pop_listint(listint_t **head)
 {
 	listint_t *ptr;
+	int n;
+
+	ptr = *head;
 
 	if (ptr == NULL)
 	{
@@ -16,9 +19,9 @@ int pop_listint(listint_t **head)
 	}
 	else
 	{
-		ptr = *head;
-		*head = (*head)->next;
+		*head = ptr->next;
 		free(ptr);
+		return (n);
 	}
 }
 
