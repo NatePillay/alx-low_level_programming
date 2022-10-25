4c880fd7c8fb028a9266667e3a0c9730ccc90278
NatePillay <nathan94pillay@gmail.com>
2022-10-25 12:18:03 -0700

    new commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free the memory
+ * @head: pointer to first memory address
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while ((*head) != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+
+	head = NULL;
+}
