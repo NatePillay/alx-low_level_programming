cfb8d44789fd11cf3ad00588e9cdffbaafde6105
NatePillay <nathan94pillay@gmail.com>
2022-10-25 06:08:10 -0700

    new commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - func to free linekd list
+ * @head: variable of head
+ * Return: nothing
+ */
+
+
+void free_listint(listint_t *head)
+{
+	listint_t *newnode;
+
+	while ((newnode = head) != NULL)
+	{
+		head = head->next;
+		free(newnode->n);
+		free(newnode);
+	}
+}
+
