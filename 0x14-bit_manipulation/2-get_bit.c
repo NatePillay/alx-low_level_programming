2a2e79bcd7badc1ba8368b9cf045069331319c7e
NatePillay <nathan94pillay@gmail.com>
2022-10-27 20:22:02 -0700

    new commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_bit - this is the function to get bit
+ * @n: var 1
+ * @index: var 2
+ * Return: return whether bit is set or unset
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int new_num;
+
+	new_num = n >> (index - 1);
+
+	return (new_num & 1);
+}
