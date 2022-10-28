6b5d1587b1feeaf3d7af75247d58da5564a6abf4
NatePillay <nathan94pillay@gmail.com>
2022-10-27 20:42:01 -0700

    new commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - function to set bit
+ * @n: var 1
+ * @index: index pos
+ * Return: set a bit using func
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+
+	*n = *n | (1 << index);
+}
