36614ae996b4735da8faaefd933d163079134687
NatePillay <90217237+NatePillay@users.noreply.github.com>
2022-10-30 09:03:59 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,72 +1,40 @@
 #include "dog.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * _strlen - function to find string length
- * @s: 1st variable
- * Return: 0 if success
- */
-int _strlen(char *s)
-{
-
-	unsigned int count = 0;
-
-	while (*s != 0)
-	{
-		count++;
-		s++;
-	}
-	return (count);
-}
-
-
-/**
- * *_strcpy - function to copy
- * @dest: var 1
- * @src: var 2
- * Return: copied
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-
-/**
- * new_dog - type def function to store new data
- * @name: var 1
- * @age: var 2
- * @owner: var 3
- * Return: pointer
+ * new_dog - this function is to create a new struct type
+ * @name: variable for name
+ * @age: variable for age
+ * @owner: varaible for owner name
+ * Return: new struct
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0, c = 0;
 	dog_t *Newdog;
 
+	Newdog = malloc(sizeof(dog_t));
 	if (Newdog == NULL)
+		return (NULL);
+
+	Newdog->age = age;
+	for (i = 0, c = 0; name[i] != '\0'; i++)
+		c++;
+	Newdog->name = malloc(sizeof(char) * (c + 1));
+	if (Newdog->name == NULL)
 	{
+		free (Newdog);
 		return (NULL);
 	}
 
-	char *xname;
-	char *yown;
-
-	xname = (char *)malloc(sizeof(char) * strlen(name) - 1);
-	yown = (char *)malloc(sizeof(char) * strlen(owner) - 1);
-
-	strcpy(xname, name);
-	strcpy(yown, owner);
-
+	for (i = 0, c = 0; owner[i] != '\0'; i++)
+		c++;
+	Newdog->owner = malloc(sizeof(char) * (c + 1));
+	if (Newdog->owner == NULL)
+	{
+		free (Newdog);
+		return (NULL);
+	}
 
 	return (Newdog);
 }
