c99f9be03607c4a5ac469b7f2d975e428c687b46
NatePillay <nathan94pillay@gmail.com>
2022-10-31 19:53:43 -0700

    new commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_textfile - we want to read this text file
+ * @filename: file name var
+ * @letters: for the count
+ * Return: the count of the letters and the contents of file
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t  n, n1;
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buf = malloc(sizeof(char) * letters);
+
+	n = read(fd, buf, letters);
+
+	n1 = write(STDOUT_FILENO, buf, n);
+	if (n != n1)
+		return (0);
+
+	free(buf);
+	close(fd);
+
+	return (n);
+}
+
