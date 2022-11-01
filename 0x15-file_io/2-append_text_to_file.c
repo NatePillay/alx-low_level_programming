08026b28d59480e31f0154f3bd2d512b82c3d0a1
NatePillay <nathan94pillay@gmail.com>
2022-10-31 20:59:53 -0700

    new commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -41,14 +41,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-
-	while (text_content[c])
-		c++;
-
-	n = write(fd, text_content, c);
-	if (n == -1)
+	
+	if(text_content)
 	{
-		return (-1);
+		while (text_content[c])
+			c++;
+
+		n = write(fd, text_content, c);
+		if (n == -1)
+		{
+			return (-1);
+		}
 	}
 
 	close(fd);
