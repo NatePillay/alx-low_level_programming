751da1ea5f5e72316c123e788c92cf90750a2120
NatePillay <nathan94pillay@gmail.com>
2023-01-14 22:39:49 -0800

    new ne wne: git

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include  "hash_tables.h"
+
+/**
+ * hash_table_create - this is creating a hash table
+ * @size: integer for size of the array
+ * Return: If an error occurs - NULL otherwise a pointer to hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table_create;
+	unsigned long int i;
+
+	/*allocated the space for the hash table*/
+	hash_table_create = malloc(sizeof(hash_table_t));
+
+	if (hash_table_create == NULL)
+		return (NULL);
+
+	hash_table_created->size = size;
+	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
+	for (i = 0; i < size; i++)
+	{
+		hash_table_create->array[i] = NULL
+	}
+
+	return (hash_table_created);
+}
