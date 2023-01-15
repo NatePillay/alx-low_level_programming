89fa96f7ed98a34d55290b2a139a45b3801148cf
NatePillay <nathan94pillay@gmail.com>
2023-01-14 22:48:22 -0800

    keep

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,6 +19,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table_created->size = size;
 	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (hash_table_created->array == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		hash_table_create->array[i] = NULL
