3bf10a175a0566e6cb386762057962331c56a1bc
NatePillay <nathan94pillay@gmail.com>
2023-02-02 21:48:32 -0800

    one for one

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,10 +11,10 @@ int sum_dlistint(dlistint_t *head)
 	int sum = 0;
 	dlistint_t *current = head;
 
-	if (head == NULL)
+	if (!head)
 		return (0);
 
-	while (current != NULL)
+	while (current)
 	{
 		sum = sum + current->n;
 		current = current->next;
