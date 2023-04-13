56a9f71ff0afb0ce51f6d675a5a65018bee1eb83
NatePillay <nathan94pillay@gmail.com>
2023-04-13 13:10:52 -0700

    new commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+#include <unistd.h>
+
+/**
+ * linear_search - searching algo
+ * @array: pointer to variable in array
+ * @size: size pf array
+ * @value: the value we searching for
+ * Return: index of -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (*(array + i) == value)
+		{
+			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		}
+
+	}
+	return (i);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int linear_search(int *array, size_t size, int value);
+
+
+#endif
