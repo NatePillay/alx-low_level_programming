fd2e5f57dddf53b9813c50081ed4fe8c603fb939
NatePillay <nathan94pillay@gmail.com>
2023-04-16 14:27:55 -0700

    new commit

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - jump searching
+ * @array: pointer to first element
+ * @size: size of array
+ * @value: value we searching for
+ * Return: value we return or nothing
+
+
+int jump_search(int *array, size_t size, int value)
