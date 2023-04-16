8db474dea3c3bd9fd6544fa98b9208bb2ec9fa36
NatePillay <nathan94pillay@gmail.com>
2023-04-16 14:03:23 -0700

    new commit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,24 +11,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i = 0;
 
-	if (!array)
+	while (array && i < size)
 	{
-		return (-1);
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		if (*(array + i) == value)
-		{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
 			return (i);
-		}
-		else
-		{
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		}
-
+		i++
 	}
 	return (-1);
 }
